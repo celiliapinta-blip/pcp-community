6dcf910d4d8eddbe2a87f2f09c18743380620ada
Comunidad_PCP <celiliapinta@gmail.com>
2026-07-01 13:29:22 -0500

    Actualizar script.js
    Agregar conexión del historial de temporadas

--- script.js
+++ script.js
@@ -10,6 +10,9 @@ const URL_RANKING =
 const URL_FAMA =
 "https://docs.google.com/spreadsheets/d/e/2PACX-1vShb4DKAUEbkv1-6pi52xScA7MBtA0T5K78A2uwVGj0o5d70u2guNiE3VGpC2CZ5nnkmKgo-vcYairQ/pub?gid=1002334291&single=true&output=csv";
 
+const URL_HISTORIAL =
+"https://docs.google.com/spreadsheets/d/e/2PACX-1vShb4DKAUEbkv1-6pi52xScA7MBtA0T5K78A2uwVGj0o5d70u2guNiE3VGpC2CZ5nnkmKgo-vcYairQ/pub?gid=1616382426&single=true&output=csv";
+
 // ---------- VARIABLES ----------
 
 let jugadores = [];
