7eb38445731fbe6a74cf201bc30c80c8a32a87b9
Comunidad_PCP <celiliapinta@gmail.com>
2026-07-01 20:36:18 -0500

    Actualizar script.js
    eliminar error en mostrar historial

--- script.js
+++ script.js
@@ -155,33 +155,6 @@ function mostrarSalonFama() {
 }
 
 /* ==========================================
-   CARGAR HISTORIAL
-========================================== */
-
-async function cargarHistorial() {
-
-    const respuesta = await fetch(URL_HISTORIAL);
-    const texto = await respuesta.text();
-
-    const filas = texto.trim().split("\n").slice(1);
-
-    historial = filas.map(fila => {
-
-        const datos = fila.split(/,|;/);
-
-        return {
-            temporada: datos[0]?.trim() || "",
-            campeon: datos[1]?.trim() || "",
-            pais: datos[2]?.trim() || ""
-        };
-
-    }).filter(h => h.temporada !== "");
-
-    mostrarHistorial();
-
-}
-
-/* ==========================================
    TOP 3
 ========================================== */
 
